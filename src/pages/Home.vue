95442072bb4119bf184333bb30b80ed52a12ad59
PurpleLightning22 <Alladdin.kurbanbaev2212@gmail.com>
2023-06-22 12:25:28 +0500

    optimize

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,9 +5,13 @@
     </div>
     <div class="home__text">
       <span>Привет!</span>
-      <h2>Я <span>{{ text }}</span></h2>
+      <h2>
+        Я <span>{{ text }}</span>
+      </h2>
       <p>
-        Я Фрилансер и Веб разработчик.  Я стремлюсь создавать захватывающие и красивые веб сайты и приложении , тщательно продуманный код и удобста для пользователю при использований моих веб приложении.
+        Я Фрилансер и Веб разработчик. Я стремлюсь создавать захватывающие и
+        красивые веб сайты и приложении , тщательно продуманный код и удобста
+        для пользователю при использований моих веб приложении.
       </p>
 
       <div class="home__links">
@@ -17,70 +21,66 @@
         </router-link>
         <router-link class="btn-hover_blue" to="/portfolio">
           <img src="../assets/images/bag.svg" alt="" />
-          <span>Портфолио</span></router-link>
+          <span>Портфолио</span></router-link
+        >
       </div>
     </div>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
   data() {
     return {
-      text: 'Алладдин Курбанбаев',
-      textArr: ['Алладдин Курбанбаев', 'Фрилансер', 'Веб разработчик'],
+      text: "Алладдин Курбанбаев",
+      textArr: ["Алладдин Курбанбаев", "Фрилансер", "Веб разработчик"],
       bool: true,
-      time: 100
-    }
+      time: 10,
+    };
   },
   computed: {
-    ...mapState(['img'])
+    ...mapState(["img"]),
   },
   methods: {
     incrementFun(arr, j) {
-      this.text += arr[j]
+      this.text += arr[j];
     },
     switchWord() {
-      let spl = this.text.split('')
-      spl.pop()
-      this.text = spl.join('')
+      let spl = this.text.split("");
+      spl.pop();
+      this.text = spl.join("");
     },
     changeWord() {
-      let i = 0
-      let j = 0
-
-
-      setInterval(() => {
-        if (this.bool == false) {
-
-          this.incrementFun(this.textArr[i], j)
-          j++
+      let i = 0;
+      let j = 0;
+      if (this.bool == false) {
+        setInterval(() => {
+          this.incrementFun(this.textArr[i], j);
+          j++;
           if (this.text.length == this.textArr[i].length) {
-            this.bool = true
-            j = 0
+            this.bool = true;
+            j = 0;
           }
-          this.time = 1000
-        } else {
-
+          this.time = 1000;
+        }, this.time);
+      } else {
+        setInterval(() => {
           if (this.text.length == 0) {
-            i++
-            this.bool = false
+            i++;
+            this.bool = false;
             if (i > this.textArr.length - 1) {
-              i = 0
+              i = 0;
             }
           } else {
-            this.switchWord()
+            this.switchWord();
           }
-        }
-      }, this.time);
-
-
-    }
+        }, 100);
+      }
+    },
   },
 
   created() {
-    this.changeWord()
-
+    this.changeWord();
   },
 };
 </script>
